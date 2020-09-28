539d663275bde64ddf9f95138049f63eb9633b13
memowii <memogl25@gmail.com>
2020-09-28 12:35:05 -0500

    Add component Sidebar

--- src/pages/Bootcamps/Bootcamps.js
+++ src/pages/Bootcamps/Bootcamps.js
@@ -3,11 +3,6 @@ import {
   Container,
   Row,
   Col,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Card,
   Pagination,
   PaginationItem,
   PaginationLink,
@@ -15,6 +10,7 @@ import {
 
 import { usePageWithoutFixedTop } from "../../hooks/usePageWithoutFixedTop";
 import { BootcampCard } from "../../components/BootcampCard";
+import { Sidebar } from "../../components/Sidebar";
 
 import image1 from "../../assets/images/image_1.jpg";
 import image2 from "../../assets/images/image_2.jpg";
@@ -27,77 +23,8 @@ export const Bootcamps = () => {
     <section className="my-5">
       <Container>
         <Row>
-          {/* SIDEBAR */}
           <Col md="4">
-            <Card body className="mb-4">
-              <h4 className="mb-3">By Location</h4>
-              <Form>
-                <Row>
-                  <Col md="6">
-                    <FormGroup>
-                      <Input
-                        type="text"
-                        name="miles"
-                        placeholder="Miles From"
-                      />
-                    </FormGroup>
-                  </Col>
-
-                  <Col md="6">
-                    <FormGroup>
-                      <Input
-                        type="text"
-                        name="zipcode"
-                        placeholder="Enter Zipcode"
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <Input
-                  type="submit"
-                  value="Find Bootcamps"
-                  className="btn btn-primary btn-block"
-                />
-              </Form>
-            </Card>
-
-            <h4>Filter</h4>
-            <Form>
-              <FormGroup>
-                <Label> Rating</Label>
-                <Input type="select" className="custom-select mb-2">
-                  <option value="any">Any</option>
-                  <option value="9">9+</option>
-                  <option value="8">8+</option>
-                  <option value="7">7+</option>
-                  <option value="6">6+</option>
-                  <option value="5">5+</option>
-                  <option value="4">4+</option>
-                  <option value="3">3+</option>
-                  <option value="2">2+</option>
-                </Input>
-              </FormGroup>
-
-              <FormGroup>
-                <Label> Budget</Label>
-                <Input type="select" className="custom-select mb-2">
-                  <option value="any">Any</option>
-                  <option value="20000">$20,000</option>
-                  <option value="15000">$15,000</option>
-                  <option value="10000">$10,000</option>
-                  <option value="8000">$8,000</option>
-                  <option value="6000">$6,000</option>
-                  <option value="4000">$4,000</option>
-                  <option value="2000">$2,000</option>
-                </Input>
-              </FormGroup>
-
-              <Input
-                type="submit"
-                value="Find Bootcamps"
-                className="btn btn-primary btn-block"
-              />
-            </Form>
+            <Sidebar />
           </Col>
 
           <Col md="8">
