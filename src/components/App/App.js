077bfd11adca5b72a454c8170213778a39902dda
memowii <memogl25@gmail.com>
2020-10-05 17:04:26 -0500

    Add page ManageAccount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ import { AddReview } from "../../pages/AddReview";
 import { AddBootcamp } from "../../pages/AddBootcamp";
 import { ManageCourses } from "../../pages/ManageCourses";
 import { AddCourse } from "../../pages/AddCourse";
+import { ManageAccount } from "../../pages/ManageAccount";
 
 export const App = () => (
   <div className="App">
@@ -32,6 +33,7 @@ export const App = () => (
           <Route exact path="/add-review" component={AddReview} />
           <Route exact path="/manage-bootcamp" component={ManageBootcamp} />
           <Route exact path="/add-course" component={AddCourse} />
+          <Route exact path="/manage-account" component={ManageAccount} />
         </Switch>
       </Layout>
     </BrowserRouter>
---
+++ src/pages/ManageAccount.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  Card,
+  CardBody,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
+import { usePageWithoutFixedTop } from "../hooks/usePageWithoutFixedTop";
+
+export const ManageAccount = () => {
+  usePageWithoutFixedTop();
+  return (
+    <section className="mt-5">
+      <Container>
+        <Row>
+          <Col md="8">
+            <Card color="white" className="py-2 px-4">
+              <CardBody>
+                <h1 className="mb-2">Manage Account</h1>
+
+                <Form>
+                  <FormGroup>
+                    <Label>Name</Label>
+                    <Input
+                      type="text"
+                      name="title"
+                      placeholder="Name"
+                      value="John Doe"
+                    />
+                  </FormGroup>
+
+                  <FormGroup>
+                    <Label>Email</Label>
+                    <Input
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      value="jdoe@gmail.com"
+                    />
+                  </FormGroup>
+
+                  <FormGroup>
+                    <Row>
+                      <Col md="6">
+                        <Input
+                          type="submit"
+                          value="Save"
+                          className="btn btn-success btn-block"
+                        />
+                      </Col>
+
+                      <Col md="6">
+                        <NavLink
+                          to="/update-password"
+                          className="btn btn-secondary btn-block"
+                        >
+                          Update Password
+                        </NavLink>
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
