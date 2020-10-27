95fc3a6578dd810b12572649aae2518c2397bb2f
memowii <memogl25@gmail.com>
2020-10-27 13:53:34 -0600

    Refactor OptionsForSpecificUser component

--- src/app/Navbar/OptionsForSpecificUser.js
+++ src/app/Navbar/OptionsForSpecificUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { cloneElement } from "react";
 // REVIEW: Check if NavLink should be a Link.
 import { useHistory, NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -17,9 +17,12 @@ import { selectUserToken } from "../../features/user/userSlice";
 export const OptionsForSpecificUser = () => {
   const token = useSelector(selectUserToken);
   const userData = getLoggedInUserData(token);
-  const history = useHistory();
 
-  const goTo = (path) => history.push(path);
+  const logOut = () => {
+    // call api to logout
+    // clean token from storage
+    // clean token from store
+  };
 
   if (!userData) {
     return (
@@ -41,12 +44,7 @@ export const OptionsForSpecificUser = () => {
   if (userData && userData.role === "user") {
     return (
       <Dropdown>
-        <DropdownItem onClick={() => goTo("/managereviews")}>
-          Manage Reviews
-        </DropdownItem>
-        <DropdownItem onClick={() => goTo("/manage-account")}>
-          Manage Account
-        </DropdownItem>
+        <DropdownItem path="/manage-reviews">Manage Reviews</DropdownItem>
       </Dropdown>
     );
   }
@@ -54,28 +52,35 @@ export const OptionsForSpecificUser = () => {
   if (userData && userData.role === "publisher") {
     return (
       <Dropdown>
-        <DropdownItem onClick={() => goTo("/manage-bootcamp")}>
-          Manage Bootcamps
-        </DropdownItem>
-        <DropdownItem onClick={() => goTo("/manage-account")}>
-          Manage Account
-        </DropdownItem>
+        <DropdownItem path="/manage-bootcamp">Manage Bootcamps</DropdownItem>
       </Dropdown>
     );
   }
 };
 
-const Dropdown = ({ children }) => (
-  <UncontrolledDropdown nav inNavbar>
-    <DropdownToggle nav caret>
-      {IconStore("faUser")} Account{" "}
-    </DropdownToggle>
-    <DropdownMenu>
-      {children}
-      <DropdownItem divider />
-      <DropdownItem to="/reviews">
-        {IconStore("faSignOutAlt")} Logout
-      </DropdownItem>
-    </DropdownMenu>
-  </UncontrolledDropdown>
-);
+const Dropdown = ({ children }) => {
+  const history = useHistory();
+  const goTo = (path) => history.push(path);
+  const { path } = children.props;
+
+  const ClonedDropdown = () =>
+    cloneElement(children, { onClick: () => goTo(path) });
+
+  return (
+    <UncontrolledDropdown nav inNavbar>
+      <DropdownToggle nav caret>
+        {IconStore("faUser")} Account{" "}
+      </DropdownToggle>
+      <DropdownMenu>
+        <ClonedDropdown />
+        <DropdownItem onClick={() => goTo("/manage-account")}>
+          Manage Account
+        </DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem to="/reviews">
+          {IconStore("faSignOutAlt")} Logout
+        </DropdownItem>
+      </DropdownMenu>
+    </UncontrolledDropdown>
+  );
+};
