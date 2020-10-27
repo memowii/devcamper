225163e9fa3cad813f7b24fa09085b4ce6b74033
memowii <memogl25@gmail.com>
2020-10-27 12:05:40 -0600

    If a user is loged in and goes to /login redirect him to /manage-account

--- src/features/user/LoginUserPage.js
+++ src/features/user/LoginUserPage.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 import { InnerLayoutWithCard } from "../../common/components/InnerLayoutWithCard";
 import { IconStore } from "../../common/components/IconStore";
 import { LoginUserForm } from "./LoginUserForm";
+import { getLoggedInUserData } from "../../common/utils";
 
 export const LoginUserPage = () => {
+  const userData = getLoggedInUserData();
+
+  if (userData) {
+    return <Redirect to="/manage-account" />;
+  }
+
   return (
     <InnerLayoutWithCard colMd="6" cardClass="p-4 mb-4">
       <h1>{IconStore("faSignInAlt")} Login</h1>
